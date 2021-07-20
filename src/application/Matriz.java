f0714586a6f872c7b78b634736d5a4b7946fc9f8
Thiago-Marinho <peres.marinho@gmail.com>
2021-07-20 16:11:32 -0400

    atualização

--- src/application/Matriz.java
+++ src/application/Matriz.java
@@ -21,52 +21,36 @@ public class Matriz {
 		}
 		System.out.println("Enter a matrix number:");
 		int ref = sc.nextInt();
-		int res = verification(mat.clone(),ref);
 		
-		/*for(int i=0; i<mat.length; i++) {
+		for(int i=0; i<mat.length; i++) {
 			for(int j=0; j<mat[i].length; j++) {
+				
+				
 				if(ref==mat[i][j]) {
-					if(0== mat[i][j-1]) {
-					System.out.print(mat[i][j-1]);//left
-					}
-					System.out.print(mat[i][j+1]);//rigth
-				}*/
-			
-		
-		
-		sc.close();
-	}
-
-	public static int verification(int matriz [][], int ref) {
-		
-		for(int i=0; i<matriz.length; i++) {
-			for(int j=0; j<matriz[i].length; j++) {
-				
-				
-				if(ref==matriz[i][j]) {
 					System.out.println("Position: "+i+","+j);
 					
 					
 					if(j > 0) {
-						System.out.println("LEFT: "+matriz[i][j-1]);//left
+						System.out.println("LEFT: "+mat[i][j-1]);//left
 					}
-					if(j < matriz[i].length-1) {
-						System.out.println("RIGTH: "+ matriz[i][j+1]);//rigth
+					if(j < mat[i].length-1) {
+						System.out.println("RIGTH: "+ mat[i][j+1]);//rigth
 					}
 					if(i > 0) {
-						System.out.println("UP: "+matriz[i-1][j]);//up
+						System.out.println("UP: "+mat[i-1][j]);//up
 					}
-					if(i < matriz.length-1) {
-						System.out.println("DOWN: "+matriz[i+1][j]);//down										
+					if(i < mat.length-1) {
+						System.out.println("DOWN: "+mat[i+1][j]);//down										
 					}
-					System.out.println("");		
-					
-				}
-					
+					System.out.println();							
+				}			
 			}
-		}
 		
-		return 0;
+			
+		sc.close();
 	}
+  }
 }
 
+
+	
